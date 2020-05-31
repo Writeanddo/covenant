92ffd9f40557973940de3977c3e437132dd4fabd
Bruno Rollin <bruno.rollin@esauce.com.br>
2020-05-31 13:50:57 -0300

    City Rain and enviroment

---
+++ Assets/Scripts/City/CityEnviroment.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RE.City
+{
+
+    public class CityEnviroment : MonoBehaviour
+    {
+        [SerializeField] Walker[] _shadowPrefabs;
+        [SerializeField] Transform _startWaypoint;
+        [SerializeField] Transform _endWaypoint;
+        [SerializeField] float maxTime = 5;
+        [SerializeField] float minTime = 2;
+
+        private Walker _spawnObject;
+        private float _time;
+        private float _spawnTime;
+
+        void Start()
+        {
+            SetRandomTime();
+            _time = minTime;
+        }
+
+        void FixedUpdate()
+        {
+
+            _time += Time.deltaTime;
+
+            if (_time >= _spawnTime)
+            {
+                SpawnObject();
+                SetRandomTime();
+            }
+
+        }
+
+        void SpawnObject()
+        {
+            _time = 0;
+            _spawnObject = SetRandomObject();
+            _spawnObject._startWaypoint = _startWaypoint;
+            _spawnObject._endWaypoint = _endWaypoint;
+            Instantiate(_spawnObject, _spawnObject._startWaypoint.position, Quaternion.identity);
+        }
+
+        private void SetRandomTime() => _spawnTime = Random.Range(minTime, maxTime);
+
+        private Walker SetRandomObject() => _shadowPrefabs[Random.Range(0, _shadowPrefabs.Length)];
+    }
+}
---
+++ Assets/Scripts/City/Walker.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace RE.City
+{
+    public class Walker : MonoBehaviour
+    {
+        [SerializeField] float _maxMoveSpeed;
+        [SerializeField] float _minMoveSpeed;
+
+        public Transform _startWaypoint;
+        public Transform _endWaypoint;
+        private float _moveSpeed;
+
+        private void Start()
+        {
+            _moveSpeed = Random.Range(_minMoveSpeed, _maxMoveSpeed);
+        }
+
+        private void FixedUpdate()
+        {
+            transform.position = Vector2.MoveTowards(transform.position, _endWaypoint.position, _moveSpeed * Time.deltaTime);
+
+            if (Vector2.Distance(transform.position, _endWaypoint.position) < 1f)
+                Destroy(gameObject);
+        }
+
+    }
+}
